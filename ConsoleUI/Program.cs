1a4ba406e666febb130842b2b83b32c36e099986
oguzhan-derin <oguzhanderinn@gmail.com>
2021-02-11 00:05:27 +0300

    New Details

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,6 +10,11 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             ProductTest();
+            CategoryTest();
+        }
+
+        private static void CategoryTest()
+        {
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
             foreach (var category in categoryManager.GetAll())
             {
@@ -21,9 +26,18 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAllByCategoryId(2))
+            var result = productManager.GetProductDetails();
+
+            if (result.Success)
             {
-                Console.WriteLine(product.ProductName);
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + " " + product.CategoryName);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
             }
         }
     }
